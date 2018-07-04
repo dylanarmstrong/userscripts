cd892bd8d0d32e67dd789d4eec95b717e93df0b7
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-04 07:04:43 -0500

    added spacebattles-popular

---
+++ README.md
@@ -0,0 +1,13 @@
+## Tampermonkey Scripts
+
+### Notes
+My personal collection of tampermonkey (greasemonkey) scripts. These can all be used independently of each other, so just grab the ones that sound interesting.
+
+### reddit-filter.js
+Adds a small (x) next to subreddit names on r/all that hides the subreddit from r/all until removed from localStorage manually. This does not currently work with domain, word, or user filtering.
+
+### reddit-redirect.js
+Redirects calls to www.reddit.com to old.reddit.com
+
+### spacebattles-popular.js
+Hides unpopular stories on spacebattles, so it's easier to go through recently updated and find good stories.
---
+++ spacebattles-popular.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         spacebattles-popular
+// @namespace    https://forums.spacebattles.com/
+// @version      0.1
+// @description  Hide unpopular stories for easier browsing of recent
+// @author       meinhimmel
+// @match        https://forums.spacebattles.com/forums/creative-writing.18/*
+// @match        https://forums.spacebattles.com/forums/worm.115/*
+// @match        https://forums.spacebattles.com/forums/original-fiction.48/*
+// @match        https://forums.spacebattles.com/forums/roleplaying-quests-story-debates.60/*
+// @match        https://forums.spacebattles.com/forums/creative-writing-archives.40/*
+// @match        https://forums.spacebattles.com/forums/the-index.63/*
+// @grant        none
+// ==/UserScript==
+
+/**
+ * Hides unpopular stories on spacebattles
+ */
+
+(function() {
+  'use strict';
+  const viewLimit = 50000;
+  const replyLimit = 100;
+  const hidden = [];
+
+  const viewEach = (element) => {
+    const viewCount = Number(element.textContent.replace(',', ''));
+    if (viewCount < viewLimit) {
+      hidden.push(element);
+    }
+  };
+
+  const replyEach = (element) => {
+    const replyCount = Number(element.textContent.replace(',', ''));
+    if (replyCount > replyLimit) {
+      if (hidden.includes(element)) {
+        hidden.remove(element);
+      }
+    }
+  };
+
+  let elements = document.querySelectorAll('dl.minor dd');
+  elements.forEach(viewEach);
+  elements = document.querySelectorAll('dl.major dd');
+  elements.forEach(replyEach);
+
+  for (let i = 0, len = hidden.length; i < len; i++) {
+    const element = hidden[i];
+    const p = element.parentNode.parentNode.parentNode;
+    if (p && p.parentNode) {
+      p.parentNode.removeChild(p);
+    }
+  }
+})();
+
