f8dc8e4ae8cbc79f274887a5eb5540e14998061b
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-04 08:42:44 -0500

    new stylish addon thingy for me

--- README.md
+++ README.md
@@ -11,3 +11,27 @@ Redirects calls to www.reddit.com to old.reddit.com
 
 ### spacebattles-popular.js
 Hides unpopular stories on spacebattles, so it's easier to go through recently updated and find good stories.
+
+### stylize.js
+Allows me to write custom styles for websites matching hosts by string or regex. Not very user friendly and you'd be best off copying the js directly into your add-on and ignoring updates.
+
+```javascript
+const csses = {
+  '((old|www)\.)?reddit\.com$': {
+    type: 'regex',
+    css: `
+      .subreddit {
+        border: 1px solid blue;
+      }
+    `
+  },
+  'old.reddit.com': {
+    type: 'string',
+    css: `
+      .subreddit {
+        border: 1px solid red;
+      }
+    `
+  }
+};
+```
---
+++ stylize.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         stylize
+// @namespace    *
+// @version      0.1
+// @description  Add custom styles to websites
+// @author       meinhimmel
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/meinhimmel/tampermonkey-scripts/master/stylize.js
+// @supportURL   https://github.com/meinhimmel/tampermonkey-scripts/issues
+// @grant        none
+// ==/UserScript==
+
+/**
+ * Allows me to write custom styles for websites matching hosts by string or regex
+ * Not sure really how to make this one usable by others, so I'd recommend forking this
+ * Or just copying it manually to your browser
+ *
+ * Example:
+ *
+ * const csses = {
+ *  '((old|www)\.)?reddit\.com$': {
+ *    type: 'regex',
+ *    css: `
+ *      .subreddit {
+ *        border: 1px solid blue;
+ *      }
+ *    `
+ *  },
+ *  'old.reddit.com': {
+ *    type: 'string',
+ *    css: `
+ *      .subreddit {
+ *        border: 1px solid red;
+ *      }
+ *    `
+ *  }
+ * }
+ */
+
+(function() {
+  'use strict';
+
+  const { host } = document.location;
+
+  const csses = {
+    '((old|www)\.)?reddit\.com$': {
+      type: 'regex',
+      css: `
+      `
+    }
+  };
+
+  const hostFilter = (key) => {
+    const o = csses[key];
+    if (o.type === 'regex') {
+      try {
+        const r = new RegExp(key);
+        return host.match(r) !== null;
+      } catch (e) {
+      }
+    } else {
+      return host === key;
+    }
+    return false;
+  };
+
+  const keys = Object.keys(csses).filter(hostFilter);
+
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const { css } = csses[keys[i]];
+    if (css) {
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.textContent = css;
+      document.head.insertBefore(style, document.head.firstChild);
+    }
+  }
+})();
+
